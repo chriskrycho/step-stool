5599845174a670871ac15222d5d5b19b94f8c8e2
Chris Krycho <chris@chriskrycho.com>
2013-05-24 22:59:53 -0400

    Start creating setup.py.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+requires = ['Markdown', 'mdx-smartypants', 'Pygments', 'PyYAML >= 3.10']
+packages = ['step-stool']
+
+try:
+    import argparse
+except ImportError:
+    requires.append('argparse')
+
+setup(
+    name='step-stool',
+    version='0.1',
+    url='http://step-stool.io',
+    py_modules='step-stool.py',
+    packages=packages
+)
+
