5210652b044928c6974429c782b7cd7335d77390
Chris Krycho <chris@chriskrycho.com>
2013-06-15 14:37:29 -0400

    Get current working directory as default when running Step Stool

--- step-stool/step-stool.py
+++ step-stool/step-stool.py
@@ -2,6 +2,7 @@ __author__ = 'Chris Krycho'
 __copyright__ = 'Copyright © 2013 Chris Krycho'
 
 # System modules
+from os import getcwd
 import argparse
 
 # Step Stool modules
@@ -15,8 +16,8 @@ def main():
     Either configure the project initially or (re)generate the site.
     '''
     args = process_args()
-    #if not config.configured(args['working-dir']) or args['setup']:
-     #   config.setup()
+    # if args.setup or not config.configured(args.cwd):
+    #     config.setup()
 
 
 def process_args():
@@ -25,13 +26,13 @@ def process_args():
     parser = argparse.ArgumentParser(prog=program_name, description=program_desc)
 
     parser.add_argument('-d', '--working-dir', help='Set the working directory for Step Stool',
-                        metavar='<working directory>')
+                        metavar='<working directory>', dest='cwd', default=getcwd())
     parser.add_argument('-m', '--config-manually', help='Configure the site manually', action='store_true')
     parser.add_argument('--setup', help='Re-run the setup command (ignored if first run in directory)',
                         action='store_true')
 
     args = parser.parse_args()
-    print(args)
+    print(args.cwd)
     return args
 
 
