fb1530f4df7066df05950f0883739230a9812123
Chris Krycho <chris@chriskrycho.com>
2013-06-15 20:50:05 -0400

    Move default settings for config.yaml into config.py.

--- step_stool/__init__.py
+++ step_stool/__init__.py
@@ -16,13 +16,14 @@ def main():
     Either configure the project initially or (re)generate the site.
     '''
     args = process_args()
-    if not config.configured(args.cwd) or args.setup:
-        config.setup()
+    if not config.configured(args.directory) or args.setup:
+        config.setup(args.directory)
     else:
-        generate_site()
+        config.get_config(args.directory)
+        generate_site(config)
 
 
-def generate_site():
+def generate_site(config):
     '''
     Generate the site:
 
@@ -39,7 +40,7 @@ def process_args():
     parser = argparse.ArgumentParser(prog=program_name, description=program_desc)
 
     parser.add_argument('-d', '--working-dir', help='Set the working directory for Step Stool',
-                        metavar='<working directory>', dest='cwd', default=getcwd())
+                        metavar='<working directory>', dest='directory', default=getcwd())
     parser.add_argument('-m', '--config-manually', help='Configure the site manually', action='store_true')
     parser.add_argument('--setup', help='Re-run the setup command (ignored if first run in directory)',
                         action='store_true')
--- step_stool/config.py
+++ step_stool/config.py
@@ -13,23 +13,63 @@ def configured(directory):
     Check whether the directory has a configuration file already. If the file
     exists, assume it has been configured.
     '''
-    config_file_name = 'config.yaml'
-    full_path = path.join(directory, config_file_name)
+    full_path = path.join(directory, 'config.yaml')
     if path.exists(full_path):
         return True
     else:
         return False
 
 
-def get_config(document):
+def get_config(directory):
+    document = path.join(directory, 'config.yaml')
     return load(document)
 
 
-def gen_config():
+def gen_config(directory):
     ''' Generate a configuration file with default values.
     '''
+    default = '''\
+# Basic site configuration
+site_name: Step Stool Demo
+root: http://step-stool.io/demo
+content_source: /Users/chris/development/personal_projects/step-stool/sample/source
+content_output: /Users/chris/development/personal_projects/step-stool/sample/generated-site
+default_template: clean
+
+# Publication configuration
+remote:
+  - push: true
+  - push_method: # must be one of ssh, sftp, or ftp-ssl
+
+git:
+  - push: false
+  - repository:
+  - push_method: # must be one of local, ssh, or https
+  - https_username: # only required if pushing via https - NOT RECOMMENDED
+  - https_password: # only required if pushing via https - NOT RECOMMENDED
+
+hg:
+  - push: false
+  - repository:
+  - push_method: # must be either local, ssh, or https
+  - https_username: # only required if pushing via https - NOT RECOMMENDED
+  - https_password: # only required if pushing via https - NOT RECOMMENDED
+
+# Detailed configuration
+archives:
+  - generate: true
+  - categories: true
+  - tags: true
+  - author: false
+'''
+
+    file_path = path.join(directory, 'config.yaml')
+    file = open(file_path, 'w')
+    file.write(default)
+    file.close()
     pass
 
 
-def setup():
+def setup(directory):
+    gen_config(directory)
     pass
